7b1197bfec8d3c9c5e82139b74b6666e4e0d62c6
Arno Baboomian <baboomian53@gmail.com>
2025-01-22 12:27:06 +0400

    [arno] changes setting.py

--- climate_bot/climate_bot/settings.py
+++ climate_bot/climate_bot/settings.py
@@ -134,6 +134,17 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+    "http://climatenet.am",
+    "https://dev.climatenet.am",
+    "http://dev.climatenet.am",
+    "https://emvnh9buoh.execute-api.us-east-1.amazonaws.com",
+]
+
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "http://localhost:3000",
@@ -144,6 +155,28 @@ CSRF_TRUSTED_ORIGINS = [
     "http://climatenet.am",
     "https://climatenet.am"
     "https://emvnh9buoh.execute-api.us-east-1.amazonaws.com",
+]
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 
 ADMIN_SITE_TITLE = "ClimateNet BOT Admin"
